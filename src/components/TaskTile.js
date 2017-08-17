0c55b899bec998c0f05b7a0f55300cfada95b3fc
Oskar Kupski <oskar.kupski@valueadd.pl>
2017-08-17 12:02:42 +0200

    Getting tasks mocked

--- src/components/TaskTile.js
+++ src/components/TaskTile.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
 
+class Task {
+    constructor(id, title) {
+        this.id = id;
+        this.title = title;
+    }
+}
+
 export default class TaskTile extends Component {
+    constructor() {
+        super();
+        this.state = {
+            tasks: this.getTasks()
+        };
+    }
+
+    getTasks() {
+        // For now it is implemented to generate random tasks
+        return Array(Math.floor(Math.random()*10) + 1).fill(null).map(function (task, i) {
+            return new Task(i, 'Task #' + i);
+        })
+    }
+
     render() {
-        const numberOfItems = Math.floor(Math.random()*10) + 1;
-        let itemsHtml = [];
-        for (let i = 0; i < numberOfItems; i++) {
-            itemsHtml.push(<li key={i}>Item {i}</li>);
-        }
+        const tasksHtml = this.state.tasks.map(function (task) {
+            return <li key={task.id}>{task.title}</li>
+        });
+
         return (
             <div className="task-tile">
                 <h4>{this.props.name}</h4>
                 <ul>
-                    {itemsHtml}
+                    {tasksHtml}
                 </ul>
                 <input type="text" placeholder="Add task"/>
                 <button>+</button>
